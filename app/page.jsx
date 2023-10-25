d157a5e9010a0a70d07e265284c812dfadcffa9a
AbdullA-Ababakre <abdullaszu@gmail.com>
2023-10-24 22:41:22 -0700

    feat: changed the title text

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,10 @@ export default function Home() {
     <div className="w-11/12 m-auto flex-col my-6">
       <div className="flex flex-row justify-start">
         <div className="flex flex-col items-start justify-center min-h-screen text-gray-800 py-4 px-4 sm:px-6 lg:px-8 w-6/12">
-          <h2
-            className={`w-full text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl text-left ${pressStart2P.className}`}
-          >
-            AI learning journey(Click burgen menu to see the projects)
-          </h2>
           <p
             className={`w-full mt-6 max-w-2xl text-center text-lg leading-7 sm:text-2xl sm:leading-9 sm:text-left lg:text-3xl ${instrumentSans.className}`}
           >
+            AI learning journey(Click burger menu to see the projects)
           </p>
         </div>
         {/* Gallery */}
